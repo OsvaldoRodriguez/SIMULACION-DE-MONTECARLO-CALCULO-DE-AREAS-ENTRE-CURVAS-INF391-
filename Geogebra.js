2fabf87f8026320a62c9b75ed9e3e0f886072c7c
Grover Osvaldo Rodriguez Apaza <rodriguezapazagroverosvaldo@gmail.com>
2023-11-22 08:48:00 -0400

    Create Geogebra.js

---
+++ Geogebra.js
@@ -0,0 +1,20 @@
+// definiendo las funciones que intervienen
+f(x) = 3*(x-((1)/(2)))^(2)+((1)/(5))
+g(x) = -3 (x-((1)/(2)))^(2)+((1)/(2))
+h(x)=((9)/(10)) * x
+// generando los puntos aleatorios en el rango [0, 1]
+l1 = Secuencia((random(),random()),f,1,n,1)
+// se guarda en la lista {1, 0} 
+// 1 si el punto esta dentro el area requerida
+// 0 si el punto no esta en el area requerida
+Lista = Secuencia(
+    Si( Elemento(y(l1),f) >= 3 * (Elemento(x(l1),f)-((1)/(2)))^(2)+((1)/(5)) &&
+        Elemento(y(l1),f) <= -3 * (Elemento(x(l1),f)-((1)/(2)))^(2)+((1)/(2)) &&
+        Elemento(y(l1),f) <= (9/10) * Elemento(x(l1),f)
+        ,1, 0) 
+    ,f, 1, n, 1)
+// sirve para graficar los puntos de otro color que estan dentro del area requerida
+verificador = secuencia(si(Elemento(Lista, f) == 1, (Elemento( x(l1), f), 
+Elemento(y(l1), f) ) , (,) ) , f, 1, n, 1)
+// calcula la proporcion para hallar el area
+respuesta = ((Suma(Lista)) / (n))
